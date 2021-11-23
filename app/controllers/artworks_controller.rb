234b040db2d272a6bd65ffae71feb397cb5cfcbb
Clement Azalbert <clementazalbert04@gmail.com>
2021-11-23 18:00:40 +0100

    quick update of artist controller

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,8 +1,4 @@
 class ArtworksController < ApplicationController
-  # def index
-  #   @artworks = Artwork.all
-  # end
-
   def new
     @artist = Artist.find(params[:artist_id])
     @artwork = Artwork.new
@@ -12,7 +8,6 @@ class ArtworksController < ApplicationController
     @artwork = Artwork.new(artwork_params)
     @artist = Artist.find(params[:artist_id])
     @artwork.artist = @artist
-    # @artwork.user = current_user
     if @artwork.save
       redirect_to dashboard_path
     else
