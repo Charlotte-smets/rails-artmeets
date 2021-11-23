d4566e011c0c085c279ffb32a4cfe7cfd41c42b1
Clement Azalbert <clementazalbert04@gmail.com>
2021-11-23 12:06:32 +0100

    artists controller - index, show, new, create actions done

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,35 @@
 class ArtistsController < ApplicationController
+  before_action :set_artist, only: [show]
+
+  def index
+    @artists = Artist.all
+  end
+
+  def show
+    @artwork = Artwork.new
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  # check if the render :new is essential in this case
+  def create
+    @artist = Artist.new(artist_params)
+    if @artist.save
+      redirect_to artist_path(@artist)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def artist_params
+    params.require(:artist).permit(:first_name, :last_name, :artist_name, :description, :address)
+  end
+
+  def set_artist
+    @artist = Artist.find(params[:id])
+  end
 end
