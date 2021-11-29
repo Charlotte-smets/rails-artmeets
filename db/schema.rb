f696fa84c36df245ea76415517b095bc5862cf55
mohammad.ewaz@sciencespo.fr <mohammad.ewaz@sciencespo.fr>
2021-11-29 16:35:18 +0100

    chatroom update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_25_104953) do
+ActiveRecord::Schema.define(version: 2021_11_29_151904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,14 @@ ActiveRecord::Schema.define(version: 2021_11_25_104953) do
     t.index ["artist_id"], name: "index_artworks_on_artist_id"
   end
 
+  create_table "chatrooms", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "match_id", null: false
+    t.index ["match_id"], name: "index_chatrooms_on_match_id"
+  end
+
   create_table "favorites", force: :cascade do |t|
     t.string "favoritable_type", null: false
     t.bigint "favoritable_id", null: false
@@ -105,6 +113,16 @@ ActiveRecord::Schema.define(version: 2021_11_25_104953) do
     t.index ["gallerist_id"], name: "index_matches_on_gallerist_id"
   end
 
+  create_table "messages", force: :cascade do |t|
+    t.string "content"
+    t.bigint "chatroom_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -113,6 +131,9 @@ ActiveRecord::Schema.define(version: 2021_11_25_104953) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -137,7 +158,10 @@ ActiveRecord::Schema.define(version: 2021_11_25_104953) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "artists", "users"
   add_foreign_key "artworks", "artists"
+  add_foreign_key "chatrooms", "matches"
   add_foreign_key "gallerists", "users"
   add_foreign_key "matches", "artists"
   add_foreign_key "matches", "gallerists"
+  add_foreign_key "messages", "chatrooms"
+  add_foreign_key "messages", "users"
 end
