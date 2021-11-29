edfea204b0eff6225734716dc04cfacd00eb0e76
Clement Azalbert <clementazalbert04@gmail.com>
2021-11-29 18:49:08 +0100

    front-end of the swipe done

--- app/javascript/controllers/swiper_controller.js
+++ app/javascript/controllers/swiper_controller.js
@@ -7,14 +7,24 @@ export default class extends Controller {
   connect() {
     const swiper = new Swiper(this.element, {
       effect: 'cards',
+      grabCursor: true,
+    });
+    const wrapper = this.element.firstElementChild;
+    const slide = wrapper.firstElementChild;
+    console.log(slide);
+
+    swiper.on('slideNextTransitionStart', function () {
+      console.log("slide is disliked");
+    });
+    swiper.on('slidePrevTransitionStart', function () {
+      console.log('slide is liked');
     });
   }
-
-
-  swiper() {
-    const swiper = document.querySelector('.swiper').swiper;
-    console.log(swiper)
-    // Now you can use all slider methods like
-    swiper.slideNext();
-  }
 }
+
+// swiper() {
+//   const swiper = document.querySelector('.swiper').swiper;
+//   console.log(swiper)
+//   // Now you can use all slider methods like
+//   swiper.slideNext();
+// }
