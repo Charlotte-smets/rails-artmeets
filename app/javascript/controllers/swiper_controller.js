0308102487a44827e189166e682d4d54322de654
Clement Azalbert <clementazalbert04@gmail.com>
2021-11-30 13:08:39 +0100

    working on swipe actions like & dislike

--- app/javascript/controllers/swiper_controller.js
+++ app/javascript/controllers/swiper_controller.js
@@ -1,26 +1,39 @@
 import { Controller } from "stimulus";
 import Swiper from 'swiper/swiper-bundle.min';
+// import { csrfToken } from "@rails/ujs";
 
 export default class extends Controller {
   static targets = []
+  // static values = { url: String }
 
   connect() {
+    // const a = this.element;
+    // const b = a.firstElementChild;
+    // const c = b.firstElementChild;
+    // console.log(this.urlValue);
     const swiper = new Swiper(this.element, {
       effect: 'cards',
       grabCursor: true,
     });
-    const wrapper = this.element.firstElementChild;
-    const slide = wrapper.firstElementChild;
-    console.log(slide);
+    // const urlSwiper = this.urlValue
+    // fetch(urlSwiper)
 
     swiper.on('slideNextTransitionStart', function () {
       console.log("slide is disliked");
+      console.log(swiper.slides[swiper.activeIndex - 1].dataset.swiperUrlValue);
+      console.log(swiper);
     });
+
     swiper.on('slidePrevTransitionStart', function () {
       console.log('slide is liked');
+      console.log(swiper.slides[swiper.activeIndex].dataset.swiperUrlValue);
+      console.log(swiper);
     });
   }
 }
+
+
+
 
 // listen to the event : swipe left(dislike) or swipe rigth (like)
 // if swipe left --> dislike artwork, remove it and save the event
